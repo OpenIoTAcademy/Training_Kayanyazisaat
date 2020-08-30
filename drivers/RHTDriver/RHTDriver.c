deae7a14a7147ce283c8300cb5d749f8ba5a9318
Umut <umut.osanmaz@gmail.com>
2020-08-30 19:38:46 +0300

    T-6 :RHT Driver

---
+++ drivers/RHTDriver/RHTDriver.c
@@ -0,0 +1,14 @@
+/*
+ * RHTDriver.c
+ */
+
+void RHTDriver_Init(void)
+{
+}
+int  RHTDriver_GetData()
+{
+}
+
+int  RHTDriver_Results()
+{
+}
---
+++ drivers/RHTDriver/RHTDriver.h
@@ -0,0 +1,12 @@
+/*
+ * RHTDriver.h
+ */
+
+#ifndef RHTDRIVER_H_
+#define RHTDRIVER_H_
+
+void RHTDriver_Init();
+int  RHTDriver_GetData();
+int  RHTDriver_Results();
+
+#endif /* RHTDRIVER_H_ */
